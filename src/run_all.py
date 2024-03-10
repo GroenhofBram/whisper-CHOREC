5739d29ce0da9a18dd432d950a486d68360b92b2
groenhofbram <groenhofbram@gmail.com>
2024-03-10 15:17:57 +0100

    add generalised and use conf matrix

--- src/run_all.py
+++ src/run_all.py
@@ -3,6 +3,7 @@ from src.sctkrun import main as sctk_run_unaligned
 from src.wav2vec2chorecjson import main as wav2vec2chorec_json
 from src.filteredalignment import main as align_filtered
 from src.sctk_run_aligned import main as sctk_run_aligned
+from src.confusion_matrix import main as conf_mat
 
 
 def main():
@@ -11,3 +12,4 @@ def main():
     wav2vec2chorec_json()
     align_filtered()
     sctk_run_aligned()
+    conf_mat()
